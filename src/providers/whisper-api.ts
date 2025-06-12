e6608911b3be436f471df070ab781a336fea9d3f
pluto <valcano103@gmail.com>
2025-06-13 02:23:00 +0900

    fix : some error in whisper-api Co-authored-by bbeast131 <bearbeast131@gmail.com>

--- src/providers/whisper-api.ts
+++ src/providers/whisper-api.ts
@@ -2,7 +2,6 @@ import config from "../config";
 
 async function transcribeWhisperApi(audioBlob: Blob): Promise<{ text: string; language: string }> {
 	const url = config.whisperServerUrl;
-
 	// FormData
 	const formData = new FormData();
 	formData.append("file", audioBlob);
@@ -16,14 +15,12 @@ async function transcribeWhisperApi(audioBlob: Blob): Promise<{ text: string; la
 
 	const headers = new Headers();
 	headers.append("Authorization", `Bearer ${config.whisperApiKey}`);
-
 	// Request options
 	const options = {
 		method: "POST",
 		body: formData,
 		headers
 	};
-
 	const response = await fetch(url, options);
 	const transcription = await response.json();
 	return transcription;
